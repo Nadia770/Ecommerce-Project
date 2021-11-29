ff9feaeb708ea5fe2d7050c9a1eb76bab55c105d
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-11-29 15:41:21 +0000

    Create Product List and Product Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation';
-// import {Router} from '@reach/router'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navigation";
+import ProductList from "./components/ProductList/ProductList";
+import { FaDivide } from "react-icons/fa";
+import { Router } from "@reach/router";
 
 function App() {
   return (
-<Navigation></Navigation>
+    <div>
+      <Navigation></Navigation>
+      <Router>
+        <ProductList path="/home"></ProductList>
+      </Router>
+    </div>
   );
 }
 
